ebb055f5eee4b04bfc6473e9f122643a0d2e80dc
NIGVUL <77237424+NIGVUL@users.noreply.github.com>
2021-11-28 13:53:57 +0300

    Sandbox

--- sandbox/client/src/components/Content/Base/Record.jsx
+++ sandbox/client/src/components/Content/Base/Record.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import './Record.css';
 const Record = ({ marginLeft, data, id}) => {
     
-    function print(id){
-        console.log(id)
-    }
     return (
             <div id={id} className='record' style={{marginLeft: marginLeft + 'px'}}>
-                <span onClick={ () => {print(id)}}>{data}</span>
+                <span>{data}</span>
             </div>
     );
 }
